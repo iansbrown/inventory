03f573487c62d77f1b23e81f330b4638495bc102
Ian Brown <isbrown0123@gmail.com>
2026-04-29 17:50:13 -0500

    included whitenoise and changed settings

--- inventory/settings.py
+++ inventory/settings.py
@@ -14,9 +14,6 @@ from pathlib import Path
 import os
 import dj_database_url
 
-DATABASES = {
-    "default": dj_database_url.config(conn_max_age=600)
-}
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -52,6 +49,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -84,10 +82,10 @@ WSGI_APPLICATION = "inventory.wsgi.application"
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///db.sqlite3',
+        conn_max_age=600
+    )
 }
 
 
@@ -126,6 +124,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
