9eb1233c104bb62752fae529b77e423b1361353a
Ian Brown <isbrown0123@gmail.com>
2026-04-29 16:41:19 -0500

    render db update
    setup postgresql in render

--- inventory/settings.py
+++ inventory/settings.py
@@ -12,6 +12,12 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
+
+DATABASES = {
+    "default": dj_database_url.config(conn_max_age=600)
+}
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,12 +27,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-pr75%!v%^q%8q9=qf6v5=13!75$$ney+orz7h$a$0so(-hyq7)"
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "insecure-staging-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DJANGO_DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
