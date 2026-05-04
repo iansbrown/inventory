2ce2acb1ea5339aa3ef88cb90b61939fbc036e0b
Ian Brown <isbrown0123@gmail.com>
2026-05-04 15:16:08 -0500

    Create __init__.py

---
+++ scheduling/models/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May  4 15:14:46 2026
+
+@author: ianbrown
+"""
+
+from .terms import AcademicTerm
+from .courses import LabCourse
+from .offerings import LabOffering
+from .weeks import ScheduleWeek
+from .meetings import WeekMeeting
+from .sections import LabSection
