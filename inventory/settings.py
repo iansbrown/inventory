9acfed2c73a1eadb35e4b5a43ba5aa8685b94fed
Ian Brown <isbrown0123@gmail.com>
2026-05-04 15:31:39 -0500

    Update settings.py

--- inventory/settings.py
+++ inventory/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "scheduling.apps.SchedulingConfig",
     
     # Your app
     "equipment",
