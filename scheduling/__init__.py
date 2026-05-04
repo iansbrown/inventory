59c0adc68a28d19e949aa1aaf16bc38498841163
Ian Brown <isbrown0123@gmail.com>
2026-05-04 15:29:07 -0500

    fixed missing files for scheduling module

---
+++ scheduling/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May  4 15:27:24 2026
+
+@author: ianbrown
+"""
+
+# scheduling/__init__.py
---
+++ scheduling/apps.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May  4 15:21:42 2026
+
+@author: ianbrown
+"""
+
+from django.apps import AppConfig
+
+
+class SchedulingConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "scheduling"
