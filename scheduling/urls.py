f11a3de937c38dfd7693157de6d98c2b9f7d30e7
Ian Brown <isbrown0123@gmail.com>
2026-05-13 14:19:26 -0500

    fix for conflicts in term view

--- scheduling/urls.py
+++ scheduling/urls.py
@@ -27,7 +27,7 @@ urlpatterns = [
     ),
 
     path(
-        "<str:course_code>/<str:term_id>/equipment/",
+        "<str:course_code>/<int:term_id>/equipment/",
         views.equipment_by_week_view,
         name="equipment_by_week",
     ),    
--- scheduling/views.py
+++ scheduling/views.py
@@ -192,7 +192,7 @@ def equipment_by_week_view(request, course_code, term_id):
     lab_offering = get_object_or_404(
         LabOffering,
         lab_course__course_code=course_code,
-        academic_term__name=term_id,
+        academic_term__id=term_id,
     )
 
     schedule_weeks = (
