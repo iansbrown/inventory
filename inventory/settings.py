d158b17ad016b3b315f4a33d15c0ea1e3c2c4107
Ian Brown <isbrown0123@gmail.com>
2026-05-13 09:11:15 -0500

    Update settings.py

--- inventory/settings.py
+++ inventory/settings.py
@@ -32,6 +32,20 @@ DEBUG = os.getenv("DJANGO_DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = ["*"]
 
+#error logging for render envioronment
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "ERROR",
+    },
+}
 
 # Application definition
 
