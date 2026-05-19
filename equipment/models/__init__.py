1aa64b551954a791f3253d40b2586a56ae8f7c35
Ian Brown <isbrown0123@gmail.com>
2026-05-19 09:36:15 -0500

    added equipment requirements to experiment dashboard

--- equipment/models/__init__.py
+++ equipment/models/__init__.py
@@ -15,3 +15,4 @@ from .repairs import RepairLog
 from .audit import ChangeLog
 from .equipment_type import EquipmentType
 from .requests import EquipmentRequest
+from .experimentreqs import ExperimentEquipmentRequirement
